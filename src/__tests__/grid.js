15cad7e01aead946586f0160b36a876a4364b275
vlad <vladpovalii@gmail.com>
2020-03-26 08:16:42 +0200

    added test

--- src/__tests__/grid.js
+++ src/__tests__/grid.js
@@ -1,22 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Grid from "../components/Grid";
-import {render, screen, act} from '@testing-library/react';
+import {render, act} from '@testing-library/react';
 
-it("renders Grid without crashing", () => {
+jest.useFakeTimers();
+
+it("it renders Grid without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Grid/>, div);
+    ReactDOM.render(<Grid isRunning={false}/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
-it("renders Grid with given dimensions", () => {
+it("it renders Grid with given dimensions", () => {
     const size = 100;
-    const { container } = render(<Grid size={size} />);
+    const { container } = render(<Grid size={size} isRunning={false} />);
     expect(container.firstChild.childNodes.length).toBe(size * size);
 });
 
-if('updated Grid with given frequency', () => {
-    const size = 100;
-    const { container } = render(<Grid size={size} />);
-    expect(container.firstChild.childNodes.length).toBe(size * size);
+it("it updates Grid with given frequency", () => {
+    const delay = 100;
+    act (() => {
+        render(<Grid isRunning={true} delay={delay}/>);
+    });
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), delay);
 });
